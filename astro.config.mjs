77da6af358f1512beb9e590a5f980048bb10561e
Orelvys Cuellar <cubanote816@gmail.com>
2026-03-29 13:38:38 +0200

    fix: change astro build assets dir from _astro to assets_astro to prevent 404 block on generic servers

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: process.env['ASTRO_SITE_URL'] || 'https://claesen-verlichting.be',
   base: process.env['ASTRO_BASE_PATH'] || '/v1',
+  build: {
+    assets: 'assets_astro'
+  },
   i18n: {
     defaultLocale: "nl",
     locales: ["nl", "en", "fr"],
